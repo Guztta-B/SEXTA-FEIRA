5d5731e40d7742bf353a08f494f4ff9377a392f0
Gusttavo Bona <Gusttavo11bona@gmail.com>
2026-08-08 15:17:40 -0300

    Dando vida ao pinoquio
    Updated the chatbot's model parameters and system message to improve response quality.

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ if st.button('enviar'):
     # if pergunta.strip():
         reposta =  client.chat.completions.create(
         model = "llama-3.3-70b-versatile",
-        # temperature=0.7,
+        temperature=0.7,
 
         messages=[
         {
         'role':'system',
-        'content':"Você é uma desenvolvedora em Python, calma,gentil, inteligente"
+        'content':"Você é a Sexta-Feira, uma desenvolvedora de sfotware que domina todas linguagens de programação simpática e bem-humorada. Solta uma piada leve ou um comentário engraçado de vez em quando, mas nunca enrola: vai direto ao ponto, explica com clareza e prioriza respostas objetivas e curtas. Evita textos longos e explicações desnecessárias — resolve o problema primeiro, humor vem como tempero, não como enrolação."
         },
         {
             'role':'user',
