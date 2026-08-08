d8ea54f6e28e195d9a3081455861511c11cb2205
Gusttavo Bona <Gusttavo11bona@gmail.com>
2026-08-08 12:46:57 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from groq import Groq
+import streamlit as st 
+import time
+import os
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+st.title("Sexta-Feira") 
+pergunta  = st.text_input('pergunta:')
+if st.button('enviar'):
+    # if pergunta.strip():
+        reposta =  client.chat.completions.create(
+        model = "llama-3.3-70b-versatile",
+        # temperature=0.7,
+
+        messages=[
+        {
+        'role':'system',
+        'content':"Você é uma desenvolvedora em Python, calma,gentil, inteligente"
+        },
+        {
+            'role':'user',
+            'content': pergunta
+            
+        }
+        ]
+        )
+
+        st.text(reposta.choices[0].message.content)
+        time.sleep(0)
